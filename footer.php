2412591245fc9ac5003628de21bf7c3df85be521
LebCit <lebcit@gmail.com>
2020-05-20 19:22:53 +0300

    Added footer to content

--- footer.php
+++ footer.php
@@ -11,23 +11,22 @@
 
 ?>
 
+		<footer id="colophon" class="site-footer">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'midday' ) ); ?>">
+					<?php
+					/* translators: %s: CMS name, i.e. WordPress. */
+					printf( esc_html__( 'Proudly powered by %s', 'midday' ), 'WordPress' );
+					?>
+				</a>
+				<span class="sep"> <br> </span>
+					<?php
+					/* translators: 1: Theme name, 2: Theme author. */
+					printf( esc_html__( 'Theme: %1$s by %2$s.', 'midday' ), 'MidDay', '<a href="https://github.com/LebCit">LebCit</a>' );
+					?>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
 	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'midday' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'midday' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'midday' ), 'midday', '<a href="https://github.com/LebCit">LebCit</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
